44394272ab6385702e6bfb82a2f58056a3581a33
Fernanda8234 <fernanda82354769@gmail.com>
2026-07-26 20:37:36 -0300

    Correções de erros e formatação de celular

--- js/pages/inicio.js
+++ js/pages/inicio.js
@@ -3,40 +3,43 @@
 import { renderizarPagina } from '../main.js'
 
 export function criarInicio(){
-    const formulario = document.createElement('form')
-    formulario.id = 'formulario'
+    const secao = document.createElement('section')
+    secao.id = 'secao'
 
     const texto = document.createElement('p')
     texto.innerHTML = 'Escolha um <span class="destaque">curso</span> para gerenciar'
     texto.id = 'texto'
 
     const dispositivos = document.createElement('img')
-    dispositivos.src = '../../img/devices.png'
+    dispositivos.src = './img/devices.png'
     dispositivos.id = 'dispositivos'
 
     const apresentacao = document.createElement('div')
     apresentacao.id = 'apresentacao'
 
     const estudante = document.createElement('img')
-    estudante.src = '../../img/studant.png'
+    estudante.src = './img/studant.png'
     estudante.id = 'estudante'
 
     const ds = document.createElement('button')
-    ds.src = '../../img/ds.png'
     ds.textContent = 'DS'
     ds.id = 'ds'
 
     const redes = document.createElement('button')
-    redes.src = '../../img/redes.png'
     redes.textContent = 'REDES'
     redes.id = 'redes'
 
     const botoes = document.createElement('div')
     botoes.id = 'botoes'
+    
+    if (window.innerWidth <= 480) {
+        ds.textContent = 'DS';
+        redes.textContent = 'RDS';
+    }
 
     apresentacao.append(texto, dispositivos)
     botoes.append(ds, redes)
-    formulario.append(apresentacao, estudante, botoes)
+    secao.append(apresentacao, estudante, botoes)
 
-    return formulario
+    return secao
 }
